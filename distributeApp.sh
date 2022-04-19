f3b356228c810b230f049bfb3d7812de1f61e8f9
gelhan <32517011+gelhan@users.noreply.github.com>
2022-04-19 17:10:43 +0200

    Update script with fix for github workflow permission issue

--- distributeApp.sh
+++ distributeApp.sh
@@ -5,7 +5,8 @@ if [[ -z ${INPUT_RELEASE_NOTES} ]]; then
     INPUT_RELEASE_NOTES="$(git log -1 --abbrev-commit --pretty=oneline)"
 fi
 
-git config --global --add safe.directory /github/workspace
+git config --global safe.directory "$GITHUB_WORKSPACE"
+git config --global safe.directory /github/workspace
 
 firebase appdistribution:distribute "$INPUT_APP_FILE" --app \
     "$INPUT_APP_ID" --token "$INPUT_FIREBASE_TOKEN" --groups "$INPUT_TESTER_GROUPS" \
